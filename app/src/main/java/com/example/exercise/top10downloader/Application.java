bdb6d97aa506b02a2e82ae9aab6d3e59d64faa5f
ygoh@noreply.github.com <ygoh@noreply>
2016-06-30 16:08:23 +0800

    top 10 download app

---
+++ app/src/main/java/com/example/exercise/top10downloader/Application.java
@@ -0,0 +1,41 @@
+package com.example.exercise.top10downloader;
+
+public class Application {
+    private String name;
+    private String artist;
+    private String releaseDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Application{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", artist='").append(artist).append('\'');
+        sb.append(", releaseDate='").append(releaseDate).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ app/src/main/java/com/example/exercise/top10downloader/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.exercise.top10downloader;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String mFileContents;
+    private Button btnParse;
+    private ListView listApps;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnParse = (Button) findViewById(R.id.btnParse);
+        btnParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: Parse XML
+                ParseApplications pa = new ParseApplications(mFileContents);
+                pa.process();
+                ArrayAdapter<Application> arrayAdapter = new ArrayAdapter<Application>(
+                        MainActivity.this,
+                        R.layout.list_item,
+                        pa.getApplications()
+                );
+                listApps.setAdapter(arrayAdapter);
+            }
+        });
+        listApps = (ListView) findViewById(R.id.xmlListView);
+
+        DownloadData dd = new DownloadData();
+        dd.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
+
+    }
+
+    private class DownloadData extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            mFileContents = downloadXML(params[0]);
+
+            if (mFileContents == null) {
+                Log.d("DownloadData", "Error downloading");
+            }
+
+            return mFileContents;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d("DownloadData", "Result was " + s);
+        }
+
+        private String downloadXML(String urlPath) {
+            StringBuilder tempBuffer = new StringBuilder();
+
+            try {
+                URL url = new URL(urlPath);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                int response = connection.getResponseCode();
+                Log.d("DownloadData", "Response code:" + response);
+                InputStream is = connection.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+
+                int charRead;
+                char [] inputBuffer = new char[500];
+
+                while (true) {
+                    charRead = isr.read(inputBuffer);
+                    if (charRead <= 0) {
+                        break;
+                    }
+                    tempBuffer.append(String.copyValueOf(inputBuffer, 0, charRead));
+                }
+
+                return tempBuffer.toString();
+            } catch (IOException e) {
+                Log.e("DownloadData", "Error downloading: " + e.getMessage());
+            } catch (SecurityException e) {
+                Log.e("DownloadData", "Need Permission: " + e.getMessage());
+            }
+
+            return null;
+        }
+    }
+}
